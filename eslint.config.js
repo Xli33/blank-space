efec2290503bb2761df71dd43c6ab85511e7dfbd
Xli33 <lshin1232@gmail.com>
2024-10-28 15:56:41 +0800

    build: 引入eslint，在pre-commit阶段执行lint检查

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  {
+    ignores: ['.husky/']
+  },
+  {
+    files: ['src/**/*.{js,mjs}'],
+    languageOptions: {
+      // ecmaVersion: 'latest', default: "latest"
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      // '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+      // 'vue/multi-word-component-names': 'off',
+      'no-prototype-builtins': 'off',
+      'no-debugger': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          // vars: 'all',
+          // args: 'after-used',
+          caughtErrors: 'none'
+          // ignoreRestSiblings: false,
+          // reportUsedIgnorePattern: false
+        }
+      ],
+      'no-empty': ['error', { allowEmptyCatch: true }]
+    }
+  }
+];
